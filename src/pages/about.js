a2541fae0a4b23ca8b5321fbe6ecccb9bac929a3
nadeeraka <nimanthanadeeraka@outlook.com>
2019-02-28 13:24:31 +0530

    1

---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default function About() {
+  return (
+    <div>
+      <Layout>
+        <h1>About</h1>
+
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis
+          amet nam fuga sequi eaque odit dolorem veniam. Molestiae voluptatibus,
+          suscipit amet alias excepturi fugiat et iusto eum, a consectetur sint.
+        </p>
+      </Layout>
+    </div>
+  )
+}
