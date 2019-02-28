e6dc89ecb2ec8961d919c6196fc9de4ac8a8da31
nadeeraka <nimanthanadeeraka@outlook.com>
2019-02-28 14:45:04 +0530

    2 commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,6 +10,7 @@ import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
+import Menu from "../pages/menu"
 import "./layout.css"
 
 const Layout = ({ children }) => (
@@ -34,6 +35,7 @@ const Layout = ({ children }) => (
             paddingTop: 0,
           }}
         >
+          <Menu />
           <main>{children}</main>
           <footer>
             © {new Date().getFullYear()}, Built with
---
+++ src/pages/menu.js
@@ -0,0 +1,23 @@
+import React from "react"
+
+export default function Menu() {
+  return (
+    <div>
+      <nav className="nav">
+        <ul>
+          <li>
+            <a href="/">Start</a>
+          </li>
+
+          <li>
+            <a href="/">Start</a>
+          </li>
+
+          <li>
+            <a href="/">Start</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
